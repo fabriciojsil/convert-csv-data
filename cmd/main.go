3ee8aad2e241ddd0b829c936e456b4cb29f4ed0b
Fabricio Jean <fabricio.silva@resultadosdigitais.com.br>
2018-05-12 16:12:07 -0300

    Addin Main

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"time"
+
+	"github.com/fabriciojsil/convert-csv-data/internal/service"
 )
 
 func main() {
-	fmt.Println("Running main")
+	start := time.Now()
+	file := flag.String("file", ".", "a string")
+	format := flag.String("format", "json", "a string")
+	flag.Parse()
+
+	service.DeliveryFile(string(*format), string(*file))
+
+	fmt.Println(time.Since(start))
 }
